318522ec4a331d31f9ed0d741b1e6b77c607585a
Song-ded <84007219+Song-ded@users.noreply.github.com>
2025-03-22 17:40:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from random import choice
 
 app = Flask(__name__)
 
-# Ваш код шифрования
 chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 numbers = [5, 7, 2, 3, 14, 9, 11, 20, 15, 18, 4, 8, 5, 6, 7, 8, 9, 10, 8, 2, 0, 4, 0, 8, 1, 6]
 cyrillic_letters = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
@@ -30,24 +29,22 @@ def encrypt_word(word_input):
             if char in cyrillic_letters:
                 word += letter_code(cyrillic_letters.index(char) + 1)
             else:
-                word += char  # Если символ не кириллический, оставляем как есть
+                word += char
         except StopIteration:
             break
     return word
 
-# Маршрут для главной страницы
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Маршрут для обработки шифрования (AJAX-запрос)
 @app.route('/encrypt', methods=['POST'])
 def encrypt():
-    data = request.json  # Получаем данные из запроса
+    data = request.json
     word_input = data.get('word')
     if not word_input:
         return jsonify({'error': 'Слово не предоставлено'}), 400
-    encrypted_word = encrypt_word(word_input)  # Шифруем слово
+    encrypted_word = encrypt_word(word_input)
     return jsonify({'encrypted_word': encrypted_word})
 
 if __name__ == '__main__':

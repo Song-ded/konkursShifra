99728470dfc5e52267f5ea36c8a663f7b851f585
Song-ded <84007219+Song-ded@users.noreply.github.com>
2025-03-23 19:32:28 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -26,3 +26,39 @@ document.getElementById('encryptButton').addEventListener('click', () => {
         document.getElementById('encryptedResult').textContent = 'Произошла ошибка. Проверьте консоль для подробностей.';
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('contestEndModal');
+    modal.style.display = 'flex';
+});
+
+document.getElementById('closeModal').addEventListener('click', () => {
+    const modal = document.getElementById('contestEndModal');
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    const modal = document.getElementById('contestEndModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+document.getElementById('themeToggle').addEventListener('click', () => {
+    const body = document.body;
+    const themeToggleIcon = document.querySelector('.theme-toggle i');
+
+    if (body.classList.contains('dark-theme')) {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        themeToggleIcon.classList.remove('fa-moon');
+        themeToggleIcon.classList.add('fa-sun');
+        localStorage.setItem('theme', 'light-theme');
+    } else {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        themeToggleIcon.classList.remove('fa-sun');
+        themeToggleIcon.classList.add('fa-moon');
+        localStorage.setItem('theme', 'dark-theme');
+    }
+});
